007810438fa1ac99dc013f84a92047409b3865be
Douglas Fergusson <J233195@tafe.wa.edu.au>
2023-10-29 15:40:13 +0800

    feat: Add settingsmanager class to manage preferences.
    feat: Added AppTheme setting and enum.

---
+++ FoodInfo/Services/SettingsManager.cs
@@ -0,0 +1,34 @@
+﻿using Java.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodInfo.Services
+{
+    internal class SettingsManager
+    {
+		static public AppTheme CurrentTheme
+		{
+			get 
+            { 
+                string themeString = Preferences.Get("AppTheme", AppTheme.System.ToString());
+                if (Enum.TryParse(themeString, out AppTheme theme))
+                {
+                    return theme;
+                }
+                return AppTheme.System;
+            }
+			set { Preferences.Set("AppTheme", value.ToString()); }
+		}
+
+    }
+
+    public enum AppTheme
+    {
+        Light,
+        Dark,
+        System
+    }
+}
