35520dec62d4f687031a2e1b5d0de91e366df7ee
D Fergusson <dfergusson@outlook.com.au>
2023-11-14 10:45:42 +0800

    Cleanup mainpage

--- FoodInfo/Views/MainView.xaml.cs
+++ FoodInfo/Views/MainView.xaml.cs
@@ -5,9 +5,6 @@ namespace FoodInfo.Views;
 
 public partial class MainView : ContentPage
 {
-	public ProductsSearchByNameResponseModel recentProducts;
-    public ProductSearchByCodeResponseModel testProductResponse;
-    public ProductModel testProduct;
 	public MainView()
 	{
 		InitializeComponent();	
@@ -15,29 +12,7 @@ public partial class MainView : ContentPage
 
     private async void ButtonScanBarcode_Clicked(object sender, EventArgs e)
     {
-		/*await Shell.Current.GoToAsync("//ProductPage");*/
-		/*AppTheme theme = Application.Current.RequestedTheme;*/ //enum
-		Application.Current.Resources.MergedDictionaries.Clear();
-		Application.Current.Resources.MergedDictionaries.Add(new LightTheme());
-
-        recentProducts = await OpenFoodFactsAPIService.GetProductsResponse("rosella");
-        testProductResponse = await OpenFoodFactsAPIService.GetProductResponse(recentProducts.Products[0].Barcode);
-        testProduct = testProductResponse.Product;
-
-
-		if(recentProducts != null)
-		{
-			RecentProductOne.Text = recentProducts.Products[0].Name;
-			RecentProductOneImage.Source = recentProducts.Products[0].ImageUrl;
-            RecentProductTwo.Text = recentProducts.Products[1].Name;
-            RecentProductTwoImage.Source = recentProducts.Products[1].ImageUrl;
-        }
-
-    }
-
-    private async void Recent_Tapped(object sender, TappedEventArgs e)
-    {
-        await Navigation.PushAsync(new ProductView(testProduct));
+        await Navigation.PushAsync(new SearchView(Entry_Search.Text));
     }
 
     private async void ButtonPreferences_Clicked(object sender, EventArgs e)
@@ -46,6 +21,11 @@ public partial class MainView : ContentPage
     }
 
     private async void Entry_Search_Completed(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new SearchView(Entry_Search.Text));
+    }
+
+    private async void Recent_Tapped(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new SearchView(Entry_Search.Text));
     }
