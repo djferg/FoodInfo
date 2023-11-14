ad3a7ec261c1cb010ed4f6779373a0549d180cf2
D FERGUSSON <J233195@tafe.wa.edu.au>
2023-11-14 12:29:27 +0800

    Allow searching by barcode

--- FoodInfo/Views/MainView.xaml.cs
+++ FoodInfo/Views/MainView.xaml.cs
@@ -22,7 +22,16 @@ public partial class MainView : ContentPage
 
     private async void Entry_Search_Completed(object sender, EventArgs e)
     {
-        await Navigation.PushAsync(new SearchView(Entry_Search.Text));
+        bool isNumber = long.TryParse(Entry_Search.Text, out _);
+        if (isNumber)
+        {
+            ProductModel product = new();
+            product.Barcode = Entry_Search.Text;
+            await Navigation.PushAsync(new ProductView(product));
+        } else
+        {
+            await Navigation.PushAsync(new SearchView(Entry_Search.Text));
+        }        
     }
 
     private async void Recent_Tapped(object sender, EventArgs e)
